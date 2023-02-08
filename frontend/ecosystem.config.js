5d1bd7600bb3f1ed49258aec878709d7c3da0e69
AlekseySobolev <sobzone@yandex.ru>
2023-02-08 23:16:09 +0300

    add ecosystem.config and crash-test rout

---
+++ frontend/ecosystem.config.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv');
+
+dotenv.config({path: '.dev.deploy'});
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/app.js',
+  }],
+
+  // Настройка деплоя
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/AlekseySobolev/web-plus-pm2-deploy',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
